9970cac90cb6768a192fdfce1d4c476ba7bb51eb
Dmitrii Badretdinov <dmitriybadretdinov@gmail.com>
2022-06-01 11:04:42 +0200

    Add a Csharp task. Main things used: string parsing, unit tests, exception handling.

---
+++ csharp_ternary/csharp_ternary/csharp_ternary/Task1.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Numerics;
+
+namespace csharp_ternary
+{
+    public class Task1
+    {
+        public string convertToCustomTernary(string input)
+        {
+            _resultingStringBuilder.Clear();
+            try
+            {
+                _inputNumber = BigInteger.Parse(input);
+            }
+            catch
+            {
+                Console.WriteLine("The input number is incorrect.");
+                throw;
+            }
+            if (_inputNumber < 0)
+            {
+                throw new Exception("The number is less than 0.");
+            }
+            if (_inputNumber == 0)
+            {
+                _resultingStringBuilder.Append(_customTernary[0]);
+            }
+            int remainder = 0;
+
+            while (_inputNumber > 0)
+            {
+                remainder = (int)(_inputNumber % 3);
+                _inputNumber /= 3;
+                _resultingStringBuilder.Append(_customTernary[remainder]);
+            }
+
+            return _resultingStringBuilder.ToString();
+        }
+
+        readonly string[] _customTernary = new string[3] { "I", "A", "V" };
+        BigInteger _inputNumber;
+        StringBuilder _resultingStringBuilder = new StringBuilder();
+    }
+}
---
+++ csharp_ternary/csharp_ternary/csharp_ternary/Task2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace csharp_ternary
+{
+    public class Task2
+    {
+        public Task2()
+        {
+            _customFromTernary = new Dictionary<char, int> {
+            { 'I', 0 },
+            { 'A', 1 },
+            { 'V', 2 }
+            };
+            StringBuilder sb = new StringBuilder();
+            foreach (char key in _customFromTernary.Keys)
+            {
+                sb.Append(key);
+            }
+            _allowedCharacters = sb.ToString();
+        }
+
+        public string ConvertFromCustomTernary(string input)
+        {
+            _resultingNumber = 0;
+            _powersOfTernaryInDecimal = new BigInteger[input.Length];
+            for (int i = 0; i < _powersOfTernaryInDecimal.Length; i++)
+            {
+                _powersOfTernaryInDecimal[i] = (BigInteger)Math.Pow(3, i);
+            }
+            CheckInputValidity(input);
+            for (int i = 0; i < input.Length; i++)
+            {
+                _resultingNumber += _customFromTernary[input[i]] * _powersOfTernaryInDecimal[i];
+            }
+            return _resultingNumber.ToString();
+        }
+
+        void CheckInputValidity(string input)
+        {
+            foreach (char c in input)
+            {
+                if (!_allowedCharacters.Contains(c))
+                {
+                    throw new Exception("Input contains forbidden characters.");
+                }
+            }
+        }
+
+        readonly Dictionary<char, int> _customFromTernary;
+        readonly string _allowedCharacters;
+        BigInteger[]? _powersOfTernaryInDecimal;
+        BigInteger _resultingNumber;
+    }
+}
